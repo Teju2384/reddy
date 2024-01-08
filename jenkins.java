3ea5bed36d34b390f3cbb6d6224f3189d550cf93
Teju2384 <152675041+Teju2384@users.noreply.github.com>
2024-01-08 22:38:05 +0530

    Add files via upload

---
+++ jenkins.java
@@ -0,0 +1,24 @@
+public class StudentInformation
+{
+public static void main(string args[])
+{
+printStudentInfo();
+}
+Private static void printStudentInfo()
+{
+String CollegeName="AITS";
+String CollegeName="teja";
+int rollnumber=1596;
+System.out.println("College Name:"+ Collegename);
+System.out.println("Student Name:"+ Studentname);
+System.out.println("Roll Number:"+ Rollnumber);
+System.out.println("\n Welcome,"+ StudentName+"!");
+System.out.println("We hope you have a fanstatic journey at"+ collegename+".");
+System.out.println("Kill the sleeping people");
+}
+}
+
+
+
+
+
